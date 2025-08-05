7186c8a201897ddc66bccdabae7738489cffdcc3
조유경 <yugyeongjo1215@gmail.com>
2025-08-05 20:26:43 +0900

    add : Q file

---
+++ bin/generated-sources/annotations/com/talktrip/talktrip/domain/chat/entity/QChatRoomMember.java
@@ -0,0 +1,45 @@
+package com.talktrip.talktrip.domain.chat.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QChatRoomMember is a Querydsl query type for ChatRoomMember
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QChatRoomMember extends EntityPathBase<ChatRoomMember> {
+
+    private static final long serialVersionUID = -543937761L;
+
+    public static final QChatRoomMember chatRoomMember = new QChatRoomMember("chatRoomMember");
+
+    public final DateTimePath<java.time.LocalDateTime> lastMemberReadTime = createDateTime("lastMemberReadTime", java.time.LocalDateTime.class);
+
+    public final StringPath lastReadMessageId = createString("lastReadMessageId");
+
+    public final StringPath memberId = createString("memberId");
+
+    public final StringPath roomId = createString("roomId");
+
+    public final StringPath roomMemberId = createString("roomMemberId");
+
+    public QChatRoomMember(String variable) {
+        super(ChatRoomMember.class, forVariable(variable));
+    }
+
+    public QChatRoomMember(Path<? extends ChatRoomMember> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QChatRoomMember(PathMetadata metadata) {
+        super(ChatRoomMember.class, metadata);
+    }
+
+}
+
